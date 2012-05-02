45849b3039fdc0d90b3d4c3756676f7fae3b4923
Mark Wienk <markwienk@gmail.com>
2012-05-02 16:20:38 +0200

    Parser

--- VB-Week2/src/vb/week2/tabular/Parser.java
+++ VB-Week2/src/vb/week2/tabular/Parser.java
@@ -1,21 +1,59 @@
 package vb.week2.tabular;
 
 public class Parser {
-	private void parseLatexTabular() {
+	protected Token currentToken;
+	protected Scanner scanner;
+	
+	protected void accept(Token.Kind expected) throws SyntaxError {
+		if(currentToken.getKind().equals(expected)) {
+			currentToken = scanner.scan();
+		} else {
+			throw new SyntaxError("Error: expected " + expected + " but received " + currentToken.getKind());
+		}
+	}
+	
+	protected void acceptIt() throws SyntaxError {
+		currentToken = scanner.scan();
+	}
+	
+	protected void parseLatexTabular() throws SyntaxError {
 		parseBeginTabular();
 		parseColsSpec();
 		parseRows();
 		parseEndTabular();
 	}
 	
-	private void parseColsSpec() {}
-	private void parseRows() {}
-	private void parseRow() {}
-	private void parseEntries() {}
+	protected void parseColsSpec() throws SyntaxError {
+		accept(Token.Kind.LCURLY);
+		parseIdentifier();
+		accept(Token.Kind.RCURLY);
+	}
 	
-	private void parseEntry() {}
-	private void parseBeginTabular() {}
-	private void parseEndTabular() {}
-	private void parseNum() {}
-	private void parseIdentifier() {}
+	protected void parseRows() {
+		try {
+			parseRow(); 
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	protected void parseRow() throws SyntaxError {
+		parseEntries();
+		accept(Token.Kind.DOUBLE_BSLASH);
+	}
+	
+	protected void parseEntries() throws SyntaxError {
+		parseEntry();
+		while(currentToken.getKind().equals(Token.Kind.AMPERSAND)) {
+			acceptIt();
+			parseEntry();
+		}
+		accept(Token.Kind.DOUBLE_BSLASH);
+	}
+	
+	protected void parseEntry() {}
+	protected void parseBeginTabular() {}
+	protected void parseEndTabular() {}
+	protected void parseNum() {}
+	protected void parseIdentifier() {}
 }
