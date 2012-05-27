d6e3a9fe63c479b9afacb9bc5cb330a147e5d04e
Mark Wienk <markwienk@gmail.com>
2012-05-27 18:16:13 +0200

    CodeGenerator (beginnetje)

--- VB-Week4/src/vb/week4/calc/Calc.java
+++ VB-Week4/src/vb/week4/calc/Calc.java
@@ -1,6 +1,6 @@
-// [file: Calc.java, started: 5-Apr-2003]
+// [file: CalcST.java, started: 5-Apr-2003]
 //
-// Driver for Calc compiler.
+// Driver for Calc compiler + Code Generator.
 //
 // CHANGES
 //      2003.04.05  ruys  Started (VB 2003).
@@ -8,8 +8,11 @@
 //                         -  AST is not longer re-built by Treeparsers.
 //                         -  CalcChecker can now throw a CalcException.
 //      2008.04.22  ruys  Migrated to ANTLR 3.
+//      2009.05.01  ruys  Added CodeGenerator.
 package vb.week4.calc;
-import java.io.FileInputStream;
+import java.io.*;
+import java.util.EnumSet;
+import java.util.Set;
 
 import org.antlr.runtime.*;             // ANTLR runtime library
 import org.antlr.runtime.tree.*;        // For ANTLR's Tree classes
@@ -18,33 +21,33 @@ import org.antlr.stringtemplate.*;      // For the DOTTreeGenerator
 /** 
  * Program that creates and starts the Calc lexer, parser, etc.
  * @author  Theo Ruys
- * @version 2008.04.22
+ * @version 2009.05.01
  */
 public class Calc {
-    private static boolean  opt_ast             = false,
-                            opt_dot             = false,
-                            opt_no_checker      = false,
-                            opt_no_interpreter  = false;
-    private static String file = null;
+    private static final Set<Option> options = EnumSet.noneOf(Option.class);
+    private static String inputFile;
     
     public static void parseOptions(String[] args) {
+        if (args.length == 0) {
+            System.err.println(USAGE_MESSAGE);
+            System.exit(1);
+        }
         for (int i=0; i<args.length; i++) {
-            if (args[i].equals("-ast"))
-                opt_ast = true;
-            else if (args[i].equals("-dot"))
-                opt_dot = true;
-            else if (args[i].equals("-no_checker"))
-                opt_no_checker = true;
-            else if (args[i].equals("-no_interpreter"))
-                opt_no_interpreter = true;
-            else if (args[i].equals("-file")) {
-            	file = args[i+1];
-            	i++;
-            }
-            else {
-                System.err.println("error: unknown option '" + args[i] + "'");
-                System.err.println("valid options: -ast -dot " +
-                                   "-no_checker -no_interpreter");
+            try {
+                Option option = getOption(args[i]);
+                if (option == null) {
+                    if (i < args.length - 1) {
+                        System.err.println("Input file name '%s' should be last argument");
+                        System.exit(1);
+                    } else {
+                        inputFile = args[i];
+                    }
+                } else {
+                options.add(option);
+                }
+            } catch (IllegalArgumentException exc) {
+                System.err.println(exc.getMessage());
+                System.err.println(USAGE_MESSAGE);
                 System.exit(1);
             }
         }
@@ -54,28 +57,38 @@ public class Calc {
         parseOptions(args);
         
         try {
-            CalcLexer lexer = new CalcLexer(new ANTLRInputStream(new FileInputStream(file)));
-            TokenStream tokens = new CommonTokenStream(lexer);
+            InputStream in = inputFile == null ? System.in : new FileInputStream(inputFile);
+            CalcLexer lexer = new CalcLexer(new ANTLRInputStream(in));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
             CalcParser parser = new CalcParser(tokens);
             
             CalcParser.program_return result = parser.program();
             CommonTree tree = (CommonTree) result.getTree();
             
-            if (! opt_no_checker) {      // check the AST
-                TreeNodeStream nodes = new CommonTreeNodeStream(tree);
+            if (!options.contains(Option.NO_CHECKER)) {      // check the AST
+                CommonTreeNodeStream nodes = new CommonTreeNodeStream(tree);
                 CalcChecker checker = new CalcChecker(nodes);
                 checker.program();
             }
 
-            if (! opt_no_interpreter) {  // interpret the AST
+            if (!options.contains(Option.NO_INTERPRETER) &&
+                    !options.contains(Option.CODE_GENERATOR)) {  // interpret the AST
                 TreeNodeStream nodes = new BufferedTreeNodeStream(tree);
                 CalcInterpreter interpreter = new CalcInterpreter(nodes);
                 interpreter.program();
             }
 
-            if (opt_ast) {          // print the AST as string
+            if (options.contains(Option.CODE_GENERATOR)) {
+                TreeNodeStream nodes = new BufferedTreeNodeStream(tree);
+                CalcCodeGenerator generator = new CalcCodeGenerator(nodes);
+                generator.program();
+            }
+
+            if (options.contains(Option.AST)) {          // print the AST as string
                 System.out.println(tree.toStringTree());
-            } else if (opt_dot) {   // print the AST as DOT specification
+            } 
+            
+            if (options.contains(Option.DOT)) {   // print the AST as DOT specification
                 DOTTreeGenerator gen = new DOTTreeGenerator(); 
                 StringTemplate st = gen.toDOT(tree); 
                 System.out.println(st);
@@ -85,7 +98,7 @@ public class Calc {
             System.err.print("ERROR: CalcException thrown by compiler: ");
             System.err.println(e.getMessage());
         } catch (RecognitionException e) {
-            System.err.print("ERROR: Recognition exception thrown by compiler: ");
+            System.err.print("ERROR: recognition exception thrown by compiler: ");
             System.err.println(e.getMessage());
             e.printStackTrace();
         } catch (Exception e) { 
@@ -94,4 +107,51 @@ public class Calc {
             e.printStackTrace();
         }
     }
+    
+    private static Option getOption(String text) throws IllegalArgumentException {
+        if (!text.startsWith(OPTION_PREFIX)) {
+            return null;
+        }
+        String stripped = text.substring(OPTION_PREFIX.length());
+        for (Option option: Option.values()) {
+            if (option.getText().equals(stripped)) {
+                return option;
+            }
+        }
+        throw new IllegalArgumentException(String.format("Illegal option value '%s'", text));
+    }
+    
+    private static final String USAGE_MESSAGE;
+    
+    static {
+        StringBuilder message = new StringBuilder("Usage:");
+        for (Option option: Option.values()) {
+            message.append(" [");
+            message.append(option.getText());
+            message.append("]");
+        }
+        message.append(" [filename]");
+        USAGE_MESSAGE = message.toString();
+    }
+
+    private static enum Option {
+        DOT,
+        AST,
+        NO_CHECKER,
+        NO_INTERPRETER,
+        CODE_GENERATOR;
+        
+        private Option() {
+            this.text = name().toLowerCase();
+        }
+        
+        /** Returns the option text of this option. */
+        public String getText() {
+            return text;
+        }
+        
+        private final String text;
+    }
+    
+    private static final String OPTION_PREFIX = "-";
 }
---
+++ VB-Week4/src/vb/week4/calc/CalcCodeGenerator.g
@@ -0,0 +1,63 @@
+tree grammar CalcCodeGenerator;
+
+options {
+  tokenVocab = Calc;
+  ASTLabelType = CommonTree;
+}
+
+@header {
+package vb.week4.calc;
+import java.util.HashMap;
+
+}
+@members {
+  private int size = 0;
+  private HashMap<String, String> vars = new HashMap<String, String>();
+  private void printTAM(String lbl, String cmd, String arg) {
+    if(!lbl.equals("") && lbl != null) lbl = lbl + ":";
+    if(cmd.equals("HALT")) System.out.format("\t\%-8s \%-8s \%8s \n", lbl, cmd, arg);
+    else System.out.format("\t\%-8s \%-8s \%8s ;\n", lbl, cmd, arg);
+  }
+  private void printTAM(String cmd, String arg) {
+    printTAM("", cmd, arg);
+  }
+}
+
+program
+    :   ^(PROGRAM (declaration | statement)+) { printTAM("POP(0)", String.valueOf(size));
+                                                printTAM("HALT", ""); }
+    ;
+    
+declaration
+    :   ^(VAR id=IDENTIFIER type)
+            { vars.put($id.text, size + "[SB]");
+              printTAM("PUSH", "1");
+              printTAM("LOAD(1)", vars.get($id.text));
+              size++; } 
+    ;
+    
+statement
+@init { int ix = input.index(); }
+    :   ^(BECOMES id=IDENTIFIER v=expr)
+            { printTAM("STORE(1)", vars.get($id.text)); }
+    |   ^(PRINT v=expr)
+            { printTAM("CALL", "putint"); }            
+    |   ^(SWAP id1=IDENTIFIER id2=IDENTIFIER)
+            { }
+    |   ^(DO ex=expr statement+)
+            { }
+    ;
+    
+expr
+    :    z=operand               {  }
+    |    ^(PLUS x=expr y=expr)   { printTAM("CALL", "add");  }
+    ;
+
+operand returns [int val = 0]
+    :   id=IDENTIFIER                       { printTAM("LOAD(1)", vars.get($id.text));      } 
+    |   n=NUMBER                            { printTAM("LOADL", String.valueOf(n)); }
+    ;
+
+type
+    :    INTEGER
+    ;
